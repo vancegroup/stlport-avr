4b2ef67480c57d43fec72ad650474f09ce35550d
ptr <ptr@09cbc127-031f-0410-9985-c3ce8c6d183e>
2000-02-22 16:12:24 +0000

    Merged changes between child workspace "/net/cosmo1/export/hostel/ptr/workshop/Helios.win" and parent workspace "/export/home/ptr/workshop/Helios".
    git-svn-id: svn://localhost/complement/trunk@463 09cbc127-031f-0410-9985-c3ce8c6d183e

--- explore/lib/mt/xmt.cc
+++ explore/lib/mt/xmt.cc
@@ -42,11 +42,13 @@ extern "C" {
 }
 
 #ifdef WIN32
+#  if 0
 extern "C" int APIENTRY
 DllMain( HINSTANCE hInstance, DWORD dwReason, LPVOID lpReserved )
 {
   return TRUE;   // ok
 }
+#  endif
 #endif
 
 namespace __impl {
