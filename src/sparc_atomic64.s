40b34f0045a1fd240f5a2f96a24ac5d4e27e5146
fbp <fbp@01b27279-081b-0410-8cf9-d9d9e0fb0389>
2001-03-30 01:52:24 +0000

    *** empty log message ***
    git-svn-id: https://stlport.svn.sourceforge.net/svnroot/stlport/trunk/STLport@64 01b27279-081b-0410-8cf9-d9d9e0fb0389

--- src/sparc_atomic64.s
+++ src/sparc_atomic64.s
@@ -12,14 +12,14 @@
 _STLP_atomic_exchange:
 1:	
 	ldx      [%o0], %o2              ! Set the current value
-        movx     %o1, %o3                ! Set the new value
+        mov      %o1, %o3                ! Set the new value
         casx     [%o0], %o2, %o3         ! Do the compare and swap
-        cmp     %o2, %o3                ! Check whether successful
-        bne     0x1b                  ! Retry upon failure
+        cmp      %o2, %o3                ! Check whether successful
+        bne      1b                  ! Retry upon failure
         membar  #LoadLoad | #LoadStore  ! Ensure the cas finishes before
                                         ! returning
         retl                            ! return
-        movx     %o2, %o0                                ! Set the new value
+        mov     %o2, %o0                                ! Set the new value
         .size   _STLP_atomic_exchange,(.-_STLP_atomic_exchange)
 
 	
@@ -29,15 +29,16 @@ _STLP_atomic_exchange:
         .global _STLP_atomic_increment
         .align  8
 _STLP_atomic_increment:
+0:	
         ldx      [%o0], %o2              ! set the current
-                add             %o2, 0x1, %o3                   ! Increment and store current
+        addx     %o2, 0x1, %o3                   ! Increment and store current
         casx     [%o0], %o2, %o3         ! Do the compare and swap
         cmp     %o3, %o2                ! Check whether successful
-                bne     _STLP_atomic_increment                                         ! Retry if we failed.
+        bne     0b
         membar  #LoadLoad | #LoadStore  ! Ensure the cas finishes before
                                         ! returning
         retl                            ! return
-        movx    %o1, %o0                                 ! Set the return value
+        mov    %o1, %o0                                 ! Set the return value
 
         .size   _STLP_atomic_increment,(.-_STLP_atomic_increment)
 
@@ -48,12 +49,12 @@ _STLP_atomic_increment:
         .align  8
 
 _STLP_atomic_decrement:
+0:	
         ldx      [%o0], %o2              ! set the current
-                sub             %o2, 0x1, %o3                   ! decrement and store current
+        subx             %o2, 0x1, %o3                   ! decrement and store current
         casx     [%o0], %o2, %o3         ! Do the compare and swap
-
         cmp     %o3, %o2                ! Check whether successful
-        bne     _STLP_atomic_decrement                                         ! Retry if we failed.
+        bne     0b
         membar  #LoadLoad | #LoadStore  ! Ensure the cas finishes before
                                         ! returning
         retl                            ! return
