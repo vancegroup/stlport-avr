401956c76412cd9233111922bdd14e428b7a2123
fbp <fbp@01b27279-081b-0410-8cf9-d9d9e0fb0389>
2001-09-08 23:10:10 +0000

    *** empty log message ***
    git-svn-id: https://stlport.svn.sourceforge.net/svnroot/stlport/trunk/STLport@171 01b27279-081b-0410-8cf9-d9d9e0fb0389

--- stlport/config/stl_gcc.h
+++ stlport/config/stl_gcc.h
@@ -58,12 +58,11 @@
 
 # endif
 
-#if /* ( defined(__linux__)  && ! defined (_GNU_SOURCE)  )  || */ \
-    defined (__CYGWIN__) || defined (__MINGW32__) || !(defined (_STLP_USE_GLIBC) || defined (__sun)) 
+#if defined (__CYGWIN__) || defined (__MINGW32__) || !(defined (_STLP_USE_GLIBC) || defined (__sun)) 
 #   define _STLP_NO_NATIVE_MBSTATE_T      1
-# endif
-
-#if defined(__linux__)
+#   define _STLP_NO_NATIVE_WIDE_FUNCTIONS 1
+#   define _STLP_NO_NATIVE_WIDE_STREAMS   1
+# elif defined(__linux__)
 #   define _STLP_NO_NATIVE_WIDE_FUNCTIONS 1
 #   define _STLP_NO_NATIVE_WIDE_STREAMS   1
 #endif
