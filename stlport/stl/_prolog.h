2daf5891bef4f1fb1c612148d1da6fbd6112150f
ptr <ptr@01b27279-081b-0410-8cf9-d9d9e0fb0389>
2004-04-06 04:58:13 +0000

    cosmetic changes
    git-svn-id: https://stlport.svn.sourceforge.net/svnroot/stlport/trunk/STLport@657 01b27279-081b-0410-8cf9-d9d9e0fb0389

--- stlport/stl/_prolog.h
+++ stlport/stl/_prolog.h
@@ -1,23 +1,25 @@
-/* NOTE : this header has no guards and is MEANT for multiple inclusion !
+/* NOTE : this header has no guards and is MEANT for multiple inclusion!
  * If you are using "header protection" option with your compiler,
  * please also find #pragma which disables it and put it here, to
  * allow reentrancy of this header.
  */
-/* We undef "std" on entry , as STLport headers may include native ones. */
-#undef std
 
-# ifdef _STLP_PROLOG_HEADER_INCLUDED
-#   error STlport prolog header can't be reincluded as long as epilog hasn't be included.
-# endif
+#ifdef std
+# undef std /* We undef "std" on entry , as STLport headers may include native ones. */
+#endif
 
-# define _STLP_PROLOG_HEADER_INCLUDED
+#ifdef _STLP_PROLOG_HEADER_INCLUDED
+#  error STlport prolog header can't be reincluded as long as epilog hasn't be included.
+#endif
 
-# ifndef _STLP_CONFIG_H
-#  include <stl/_config.h>
-# endif
+#define _STLP_PROLOG_HEADER_INCLUDED
+
+#ifndef _STLP_CONFIG_H
+# include <stl/_config.h>
+#endif
 
 /* If the platform provides any specific prolog actions,
  * like #pragmas, do include platform-specific prolog file */
-# if defined (_STLP_HAS_SPECIFIC_PROLOG_EPILOG)
-#  include <config/_prolog.h>
-# endif
+#if defined (_STLP_HAS_SPECIFIC_PROLOG_EPILOG)
+# include <config/_prolog.h>
+#endif
