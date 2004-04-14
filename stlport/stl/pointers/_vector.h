2a478b211569084b2cdd24ef4ebb1ce0defde6d0
ptr <ptr@01b27279-081b-0410-8cf9-d9d9e0fb0389>
2004-04-14 11:41:39 +0000

    missed return
    git-svn-id: https://stlport.svn.sourceforge.net/svnroot/stlport/trunk/STLport@676 01b27279-081b-0410-8cf9-d9d9e0fb0389

--- stlport/stl/pointers/_vector.h
+++ stlport/stl/pointers/_vector.h
@@ -132,7 +132,7 @@ public:
 #else
   iterator insert(iterator __pos, const_reference __x)
 #endif /*!_STLP_DONT_SUP_DFLT_PARAM && !_STLP_NO_ANACHRONISMS*/
-  { _M_impl.insert(__pos, __x); }
+  { return _M_impl.insert(__pos, __x); }
 
 #if defined(_STLP_DONT_SUP_DFLT_PARAM) && !defined(_STLP_NO_ANACHRONISMS)
   void push_back() { _M_impl.push_back(); }
@@ -154,8 +154,8 @@ public:
   
   void pop_back() { _M_impl.pop_back(); }
   
-  iterator erase(iterator __pos) { _M_impl.erase(__pos); }
-  iterator erase(iterator __first, iterator __last) { _M_impl.erase(__first, __last); }
+  iterator erase(iterator __pos) { return _M_impl.erase(__pos); }
+  iterator erase(iterator __first, iterator __last) { return _M_impl.erase(__first, __last); }
 
 #if !defined(_STLP_DONT_SUP_DFLT_PARAM)
   void resize(size_type __new_size, const_reference __x = _STLP_DEFAULT_CONSTRUCTED(_Tp))
