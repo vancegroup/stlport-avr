85dfc7a39fee0cad8ca37cdecfe76065248ef476
ptr <ptr@01b27279-081b-0410-8cf9-d9d9e0fb0389>
2004-04-20 11:03:39 +0000

    same as for gcc
    git-svn-id: https://stlport.svn.sourceforge.net/svnroot/stlport/trunk/STLport@697 01b27279-081b-0410-8cf9-d9d9e0fb0389

--- stlport/config/stl_icc.h
+++ stlport/config/stl_icc.h
@@ -2,30 +2,32 @@
 // It is internal STLport header - DO NOT include it directly
 // A list of Intel compiler for Linux portion of STLport settings.
 
-# define _STLP_LONG_LONG long long
+#define _STLP_LONG_LONG long long
 
 // Edit relative path below (or put full path) to get native
 // compiler headers included. Default is "../include".
 // C headers may reside in different directory, so separate macro is provided.
-# if (__INTEL_COMPILER >= 800)
-
+#if (__INTEL_COMPILER >= 800)
 # define _STLP_NATIVE_INCLUDE_PATH ../include/c++
 # define _STLP_NATIVE_C_INCLUDE_PATH ../include
 # define _STLP_NATIVE_CPP_C_INCLUDE_PATH ../include
 
-# ifndef __GNUC__ 
+# ifndef __GNUC__
 //  If GCC compatability is diabled (-no-gcc is specified) STD needs to be redefined.
-# define _STLP_REDEFINE_STD 1
+#  define _STLP_REDEFINE_STD 1
 # endif /* __GNUC__ */
 
-#define _STLP_HAS_NO_NEW_C_HEADERS 1
+# define _STLP_HAS_NO_NEW_C_HEADERS 1
 
-# endif /* __INTEL_COMPILER >= 800 */
+#endif /* __INTEL_COMPILER >= 800 */
 
-# undef _STLP_WINCE
+//#undef _STLP_WINCE
 
-# ifndef __GNUC__ 
+#ifndef __GNUC__ 
 # define __GNUC__ 3
-# endif
+#endif
 
-# define _STLP_NO_NATIVE_WIDE_FUNCTIONS 1
+//#define _STLP_NO_NATIVE_WIDE_FUNCTIONS 1
+
+#define _STLP_NO_FORCE_INSTANTIATE
+//#define _REENTRANT
