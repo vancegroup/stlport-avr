8adbef2962f0bdb1bf53dcbb1676bf828e437109
dums <dums@01b27279-081b-0410-8cf9-d9d9e0fb0389>
2005-12-08 21:31:05 +0000

    Add throw() specification on auto_ptr methods
    git-svn-id: https://stlport.svn.sourceforge.net/svnroot/stlport/trunk/STLport@1941 01b27279-081b-0410-8cf9-d9d9e0fb0389

--- stlport/stl/_auto_ptr.h
+++ stlport/stl/_auto_ptr.h
@@ -48,82 +48,77 @@ template<class _Tp>
 class auto_ptr :  public __ptr_base {
 public:
   typedef _Tp element_type;
-  typedef auto_ptr<_Tp>           _Self;
+  typedef auto_ptr<_Tp> _Self;
 
-  _Tp* release() {
+  _Tp* release() _STLP_NOTHROW {
     _Tp* __px = this->get();
     this->_M_p = 0;
     return __px;
   }
 
-  void reset(_Tp* __px=0) {
+  void reset(_Tp* __px = 0) _STLP_NOTHROW {
     _Tp* __pt = this->get();
     if (__px != __pt)
       delete __pt;
     this->__set(__px);
   }
 
-  _Tp* get() const { return __REINTERPRET_CAST(_Tp*,__CONST_CAST(void*,_M_p)); }
+  _Tp* get() const _STLP_NOTHROW
+  { return __REINTERPRET_CAST(_Tp*,__CONST_CAST(void*,_M_p)); }
 
-# if !defined (_STLP_NO_ARROW_OPERATOR)
-  _Tp* operator->() const {
+#if !defined (_STLP_NO_ARROW_OPERATOR)
+  _Tp* operator->() const _STLP_NOTHROW {
     _STLP_VERBOSE_ASSERT(get()!=0, _StlMsg_AUTO_PTR_NULL)
     return get();
   }
-# endif
-  _Tp& operator*() const  {
-    _STLP_VERBOSE_ASSERT(get()!=0, _StlMsg_AUTO_PTR_NULL)
+#endif
+  _Tp& operator*() const _STLP_NOTHROW {
+    _STLP_VERBOSE_ASSERT(get()!= 0, _StlMsg_AUTO_PTR_NULL)
     return *get();
   }
 
-  auto_ptr() { this->_M_p = 0; }
-
-  explicit auto_ptr(_Tp* __px) { this->__set(__px); }
+  explicit auto_ptr(_Tp* __px = 0) _STLP_NOTHROW { this->__set(__px); }
 
 #if defined (_STLP_MEMBER_TEMPLATES)
-# if !defined (_STLP_NO_TEMPLATE_CONVERSIONS)
-  template<class _Tp1> auto_ptr(auto_ptr<_Tp1>& __r) {
+#  if !defined (_STLP_NO_TEMPLATE_CONVERSIONS)
+  template<class _Tp1> auto_ptr(auto_ptr<_Tp1>& __r) _STLP_NOTHROW {
     _Tp* __conversionCheck = __r.release();
     this->__set(__conversionCheck);
   }
-# endif
-  template<class _Tp1> auto_ptr<_Tp>& operator=(auto_ptr<_Tp1>& __r) {
+#  endif
+  template<class _Tp1> auto_ptr<_Tp>& operator=(auto_ptr<_Tp1>& __r) _STLP_NOTHROW {
     _Tp* __conversionCheck = __r.release();
     reset(__conversionCheck);
     return *this;
   }
-#endif /* _STLP_MEMBER_TEMPLATES */
+#endif
 
-  auto_ptr(_Self& __r) { this->__set(__r.release()); }
+  auto_ptr(_Self& __r) _STLP_NOTHROW { this->__set(__r.release()); }
 
-  _Self& operator=(_Self& __r)  {
+  _Self& operator=(_Self& __r) _STLP_NOTHROW {
     reset(__r.release());
     return *this;
   }
 
-  ~auto_ptr() { /* boris : reset(0) might be better */ delete this->get(); }
+  ~auto_ptr() _STLP_NOTHROW { /* boris : reset(0) might be better */ delete this->get(); }
 
-  auto_ptr(auto_ptr_ref<_Tp> __r) {
-    this->__set(__r.release());
-  }
+  auto_ptr(auto_ptr_ref<_Tp> __r) _STLP_NOTHROW
+  { this->__set(__r.release()); }
 
-  _Self& operator=(auto_ptr_ref<_Tp> __r) {
+  _Self& operator=(auto_ptr_ref<_Tp> __r) _STLP_NOTHROW {
     reset(__r.release());
     return *this;
   }
 
-# if defined(_STLP_MEMBER_TEMPLATES) && !defined(_STLP_NO_TEMPLATE_CONVERSIONS)
-  template<class _Tp1> operator auto_ptr_ref<_Tp1>() {
-    return auto_ptr_ref<_Tp1>(*this, this->get());
-  }
-  template<class _Tp1> operator auto_ptr<_Tp1>() {
-    return auto_ptr<_Tp1>(release());
-  }
-# else
-  operator auto_ptr_ref<_Tp>()
+#if defined(_STLP_MEMBER_TEMPLATES) && !defined(_STLP_NO_TEMPLATE_CONVERSIONS)
+  template<class _Tp1> operator auto_ptr_ref<_Tp1>() _STLP_NOTHROW
+  { return auto_ptr_ref<_Tp1>(*this, this->get()); }
+  template<class _Tp1> operator auto_ptr<_Tp1>() _STLP_NOTHROW
+  { return auto_ptr<_Tp1>(release()); }
+#else
+  operator auto_ptr_ref<_Tp>() _STLP_NOTHROW
   { return auto_ptr_ref<_Tp>(*this, this->get()); }
-# endif
-
+#endif
 };
 _STLP_END_NAMESPACE
 
@@ -132,4 +127,3 @@ _STLP_END_NAMESPACE
 // Local Variables:
 // mode:C++
 // End:
-
