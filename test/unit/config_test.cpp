7b5440b173422f3b8b8cb6630f07733d539d1a6e
dums <dums@01b27279-081b-0410-8cf9-d9d9e0fb0389>
2006-04-05 20:45:36 +0000

    Add test case for template function partial order support configuration
    git-svn-id: https://stlport.svn.sourceforge.net/svnroot/stlport/trunk/STLport@2246 01b27279-081b-0410-8cf9-d9d9e0fb0389

--- test/unit/config_test.cpp
+++ test/unit/config_test.cpp
@@ -1,9 +1,14 @@
 #include <new>
+#include <vector>
 
 #include "cppunit/cppunit_proxy.h"
 
 //Specific STLport test case:
 #if defined (STLPORT)
+
+#  if defined (_STLP_USE_NAMESPACES)
+using namespace std;
+#  endif
 
 //
 // TestCase class
@@ -13,15 +18,16 @@ class ConfigTest : public CPPUNIT_NS::TestCase
   CPPUNIT_TEST_SUITE(ConfigTest);
   CPPUNIT_TEST(placement_new_bug);
   CPPUNIT_TEST(endianess);
+  CPPUNIT_TEST(template_function_partial_ordering);
   CPPUNIT_TEST_SUITE_END();
 
   protected:
     void placement_new_bug();
     void endianess();
+    void template_function_partial_ordering();
 };
 
 CPPUNIT_TEST_SUITE_REGISTRATION(ConfigTest);
-
 
 void ConfigTest::placement_new_bug()
 {
@@ -48,6 +54,24 @@ void ConfigTest::endianess()
 #  elif defined (_STLP_LITTLE_ENDIAN)
   CPPUNIT_ASSERT( *ptr == 0x04 );
 #  endif
+}
+
+void ConfigTest::template_function_partial_ordering()
+{
+  vector<int> vect1(10, 0);
+  int* pvect1Front = &vect1.front();
+  vector<int> vect2(10, 0);
+  int* pvect2Front = &vect2.front();
+
+  swap(vect1, vect2);
+
+#if defined (_STLP_FUNCTION_TMPL_PARTIAL_ORDER) || defined (_STLP_USE_PARTIAL_SPEC_WORKAROUND)
+  CPPUNIT_ASSERT( pvect1Front == &vect2.front() );
+  CPPUNIT_ASSERT( pvect2Front == &vect1.front() );
+#else
+  CPPUNIT_ASSERT( pvect1Front != &vect2.front() );
+  CPPUNIT_ASSERT( pvect2Front != &vect1.front() );
+#endif
 }
 
 #endif
