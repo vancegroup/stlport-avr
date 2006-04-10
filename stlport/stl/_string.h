4c55805ab01773b8d259fb746a1632fd36344c91
dums <dums@01b27279-081b-0410-8cf9-d9d9e0fb0389>
2006-04-10 19:37:22 +0000

    Replaced memory include with _alloc.h and _function.h with _function_base.h
    git-svn-id: https://stlport.svn.sourceforge.net/svnroot/stlport/trunk/STLport@2259 01b27279-081b-0410-8cf9-d9d9e0fb0389

--- stlport/stl/_string.h
+++ stlport/stl/_string.h
@@ -19,16 +19,16 @@
 #ifndef _STLP_INTERNAL_STRING_H
 #define _STLP_INTERNAL_STRING_H
 
-#ifndef _STLP_MEMORY
-#  include <memory>
+#ifndef _STLP_INTERNAL_ALLOC_H
+#  include <stl/_alloc.h>
 #endif
 
 #ifndef _STLP_STRING_FWD_H
 #  include <stl/_string_fwd.h>
 #endif
 
-#ifndef _STLP_INTERNAL_FUNCTION_H
-#  include <stl/_function.h>
+#ifndef _STLP_INTERNAL_FUNCTION_BASE_H
+#  include <stl/_function_base.h>
 #endif
 
 #ifndef _STLP_INTERNAL_ALGOBASE_H
@@ -37,6 +37,10 @@
 
 #ifndef _STLP_INTERNAL_ITERATOR_H
 #  include <stl/_iterator.h>
+#endif
+
+#ifndef _STLP_INTERNAL_UNINITIALIZED_H
+#  include <stl/_uninitialized.h>
 #endif
 
 #if defined (_STLP_USE_TEMPLATE_EXPRESSION)
