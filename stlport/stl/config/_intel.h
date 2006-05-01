0bb9029eaa343a9ad6dc15dd1b296ca28a87798b
dums <dums@01b27279-081b-0410-8cf9-d9d9e0fb0389>
2006-05-01 20:58:40 +0000

    Fix Intel C++ 9.0 compiler configuration with and without /Qvc6 option.
    git-svn-id: https://stlport.svn.sourceforge.net/svnroot/stlport/trunk/STLport@2313 01b27279-081b-0410-8cf9-d9d9e0fb0389

--- stlport/stl/config/_intel.h
+++ stlport/stl/config/_intel.h
@@ -11,11 +11,34 @@
 
 #if defined (_STLP_DONT_RETURN_VOID)
 #  undef _STLP_DONT_RETURN_VOID
-#endif /*_STLP_DONT_RETURN_VOID*/
+#endif
+
+#if (__ICL >= 900)
+#  undef _STLP_NO_UNEXPECTED_EXCEPT_SUPPORT
+#  if !defined (_STLP_DONT_USE_EXCEPTIONS)
+#    define _STLP_NOTHROW throw()
+#  endif
+#endif
+
+#if (__ICL < 450)
+/*    only static STLport lib works for ICL */
+#  undef  _STLP_USE_STATIC_LIB
+#  undef  _STLP_USE_DYNAMIC_LIB
+#  define _STLP_USE_STATIC_LIB
+/*    disable hook which makes template symbols to be searched for in the library */
+#  undef _STLP_NO_CUSTOM_IO
+#endif
 
 #undef  _STLP_LONG_LONG
 #define _STLP_LONG_LONG long long
 
 #if (__ICL >= 800)
 #  define _STLP_STATIC_CONST_INIT_BUG 1
+#endif
+
+#if defined (__cplusplus) && (__ICL >= 900) && (_MSC_VER < 1300)
+namespace std
+{
+  void _STLP_CALL unexpected();
+}
 #endif
