7a663fbe66f1cbdafa0accdf4fe54f0e41782d81
dums <dums@01b27279-081b-0410-8cf9-d9d9e0fb0389>
2006-12-20 13:43:33 +0000

    New test case for the istrstream class.
    git-svn-id: https://stlport.svn.sourceforge.net/svnroot/stlport/trunk/STLport@2835 01b27279-081b-0410-8cf9-d9d9e0fb0389

---
+++ test/unit/strstream_test.cpp
@@ -0,0 +1,40 @@
+#include <string>
+
+#if !defined (STLPORT) || !defined (_STLP_USE_NO_IOSTREAMS)
+#  include <strstream>
+
+#  include "cppunit/cppunit_proxy.h"
+
+#  if !defined (STLPORT) || defined(_STLP_USE_NAMESPACES)
+using namespace std;
+#  endif
+
+//
+// TestCase class
+//
+class StrstreamTest : public CPPUNIT_NS::TestCase
+{
+  CPPUNIT_TEST_SUITE(StrstreamTest);
+  CPPUNIT_TEST(input);
+  CPPUNIT_TEST_SUITE_END();
+
+private:
+  void input();
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION(StrstreamTest);
+
+//
+// tests implementation
+//
+void StrstreamTest::input()
+{
+#  if defined (STLPORT) && defined (_STLP_LONG_LONG)
+  istrstream is("65220830736357450");
+  _STLP_LONG_LONG rval = 0;
+  is >> rval;
+  CPPUNIT_ASSERT( rval == 65220830736357450 );
+}
+#  endif
+
+#endif
