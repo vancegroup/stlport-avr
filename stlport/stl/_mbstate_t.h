7b4e6514149a3c704824b1d52af1bdf6ee7016f9
dums <dums@01b27279-081b-0410-8cf9-d9d9e0fb0389>
2007-01-05 21:18:13 +0000

    Attempt to support compilers coming with a native library having a mbstate_t definition only for C++ compilers.
    git-svn-id: https://stlport.svn.sourceforge.net/svnroot/stlport/trunk/STLport@2845 01b27279-081b-0410-8cf9-d9d9e0fb0389

--- stlport/stl/_mbstate_t.h
+++ stlport/stl/_mbstate_t.h
@@ -16,19 +16,21 @@
 #ifndef _STLP_INTERNAL_MBSTATE_T
 #define _STLP_INTERNAL_MBSTATE_T
 
-#if (defined (__OpenBSD__) || defined (__FreeBSD__)) && defined (__GNUC__) && !defined (_GLIBCPP_HAVE_MBSTATE_T)
-#  define __mbstate_t_defined /* mbstate_t defined in native <cwchar>, so not defined in C! */
+#if (defined (__OpenBSD__) || defined (__FreeBSD__) || defined (__hpux)) && defined (__GNUC__) && !defined (_GLIBCPP_HAVE_MBSTATE_T)
+#  define _STLP_CPP_MBSTATE_T /* mbstate_t defined in native <cwchar>, so not defined in C! */
 #endif
 
-#if defined (_STLP_NO_NATIVE_MBSTATE_T) && !defined (_STLP_NO_MBSTATE_T) && !defined (_MBSTATE_T) && !defined (__mbstate_t_defined)
+#if defined (_STLP_NO_NATIVE_MBSTATE_T) || defined (_STLP_CPP_MBSTATE_T) && !defined (_STLP_NO_MBSTATE_T) && !defined (_MBSTATE_T)
 #  define _STLP_USE_OWN_MBSTATE_T
 #  define _MBSTATE_T
 #endif
 
 #if defined (_STLP_USE_OWN_MBSTATE_T)
+#  if !defined (_STLP_CPP_MBSTATE_T) || !defined (__cplusplus)
 typedef int mbstate_t;
+#  endif
 
-#  if defined (__cplusplus) && defined (_STLP_USE_NAMESPACES)
+#  if !defined (_STLP_CPP_MBSTATE_T) && defined (__cplusplus) && defined (_STLP_USE_NAMESPACES)
 _STLP_BEGIN_NAMESPACE
 using ::mbstate_t;
 _STLP_END_NAMESPACE
