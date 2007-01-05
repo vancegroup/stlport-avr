9cc9a4fb260b68e597f3f9d0fe5911004aaa13c6
dums <dums@01b27279-081b-0410-8cf9-d9d9e0fb0389>
2007-01-05 21:08:55 +0000

    Add _STLP_NON_TYPE_TMPL_PARAM_BUG macro check before doing some tests depending on it.
    git-svn-id: https://stlport.svn.sourceforge.net/svnroot/stlport/trunk/STLport@2843 01b27279-081b-0410-8cf9-d9d9e0fb0389

--- test/unit/bitset_test.cpp
+++ test/unit/bitset_test.cpp
@@ -30,18 +30,19 @@ void BitsetTest::bitset1()
 {
   bitset<13U> b1(0xFFFF);
   bitset<13U> b2(0x1111);
-  CPPUNIT_ASSERT(b1.size()==13);
-  CPPUNIT_ASSERT(b1==0x1FFF);
-  CPPUNIT_ASSERT(b2.size()==13);
-  CPPUNIT_ASSERT(b2==0x1111);
+  CPPUNIT_ASSERT(b1.size() == 13);
+  CPPUNIT_ASSERT(b1 == 0x1FFF);
+  CPPUNIT_ASSERT(b2.size() == 13);
+  CPPUNIT_ASSERT(b2 == 0x1111);
 
-  b1 = b1^(b2<<2);
-  CPPUNIT_ASSERT(b1==0x1BBB);
+#if !defined (STLPORT) || !defined (_STLP_NON_TYPE_TMPL_PARAM_BUG)
+  b1 = b1 ^ (b2 << 2);
+  CPPUNIT_ASSERT(b1 == 0x1BBB);
 
-  CPPUNIT_ASSERT(b1.count()==10);
-  CPPUNIT_ASSERT(b2.count()==4);
+  CPPUNIT_ASSERT(b1.count() == 10);
+  CPPUNIT_ASSERT(b2.count() == 4);
 
-#if defined (STLPORT) && !defined (_STLP_NO_EXTENSIONS)
+#  if defined (STLPORT) && !defined (_STLP_NO_EXTENSIONS)
   size_t __pos = b2._Find_first();
   CPPUNIT_ASSERT( __pos == 0 );
   __pos = b2._Find_next(__pos);
@@ -52,5 +53,6 @@ void BitsetTest::bitset1()
   CPPUNIT_ASSERT( __pos == 12 );
   __pos = b2._Find_next(__pos);
   CPPUNIT_ASSERT( __pos == 13 );
+#  endif
 #endif
 }
