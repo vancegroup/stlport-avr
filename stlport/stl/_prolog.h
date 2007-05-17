9083e1c7d98bc15681ba6d60b9531c7ec846e645
dums <dums@01b27279-081b-0410-8cf9-d9d9e0fb0389>
2007-05-17 08:49:21 +0000

    Hide _debug.h file when including C Standard headers as C headers are sometimes included in an 'extern "C"' context not accepting templates of _debug.h.
    git-svn-id: https://stlport.svn.sourceforge.net/svnroot/stlport/trunk/STLport@3024 01b27279-081b-0410-8cf9-d9d9e0fb0389

--- stlport/stl/_prolog.h
+++ stlport/stl/_prolog.h
@@ -26,6 +26,11 @@
 
 /* Get all debug things, potentially only empty macros if none of
  * the debug features available in user config file is activated. */
-#if defined (__cplusplus) && !defined (_STLP_DEBUG_H)
+ /* Thanks to _STLP_OUTERMOST_HEADER_ID we hide _debug.h when C standard
+  * headers are included as some platforms (Win32) include C standard headers
+  * in an 'extern "C"' scope which do not accept the templates exposed
+  * in _debug.h. */
+#if defined (__cplusplus) && !defined (_STLP_DEBUG_H) && \
+   !((_STLP_OUTERMOST_HEADER_ID >= 0x200) && (_STLP_OUTERMOST_HEADER_ID <= 0x300))
 #  include <stl/debug/_debug.h>
 #endif
