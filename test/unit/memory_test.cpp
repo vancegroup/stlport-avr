6296cc6f09ac0127184f456de6c797bdd862de3f
dums <dums@01b27279-081b-0410-8cf9-d9d9e0fb0389>
2007-08-13 19:39:17 +0000

    Ignore auto_ptr tests for MSVC6.
    git-svn-id: https://stlport.svn.sourceforge.net/svnroot/stlport/trunk/STLport@3124 01b27279-081b-0410-8cf9-d9d9e0fb0389

--- test/unit/memory_test.cpp
+++ test/unit/memory_test.cpp
@@ -12,6 +12,9 @@ using namespace std;
 class MemoryTest : public CPPUNIT_NS::TestCase
 {
   CPPUNIT_TEST_SUITE(MemoryTest);
+#if defined (_STLP_MSVC) && (_STLP_MSVC < 1310)
+  CPPUNIT_IGNORE;
+#endif
   CPPUNIT_TEST(auto_ptr_test);
   CPPUNIT_TEST_SUITE_END();
 
@@ -21,17 +24,20 @@ protected:
 
 CPPUNIT_TEST_SUITE_REGISTRATION(MemoryTest);
 
+#if !defined (_STLP_MSVC) || (_STLP_MSVC >= 1310)
 auto_ptr<int> CreateAutoPtr(int val)
 { return auto_ptr<int>(new int(val)); }
 
 bool CheckEquality(auto_ptr<int> pint, int val)
 { return *pint == val; }
+#endif
 
 //
 // tests implementation
 //
 void MemoryTest::auto_ptr_test()
 {
+#if !defined (_STLP_MSVC) || (_STLP_MSVC >= 1310)
   {
     auto_ptr<int> pint(new int(1));
     CPPUNIT_ASSERT( *pint == 1 );
@@ -57,4 +63,5 @@ void MemoryTest::auto_ptr_test()
     auto_ptr<const volatile int> pint(new int(2));
     CPPUNIT_ASSERT( *pint == 2 );
   }
+#endif
 }
