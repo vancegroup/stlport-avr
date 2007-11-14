a64f336d729baa2805a73edeaf7fec2e7bf452d3
complement <complement@01b27279-081b-0410-8cf9-d9d9e0fb0389>
2007-11-14 06:12:31 +0000

    testcase from bug report #1826271
    git-svn-id: https://stlport.svn.sourceforge.net/svnroot/stlport/trunk/STLport@3243 01b27279-081b-0410-8cf9-d9d9e0fb0389

--- test/unit/pair_test.cpp
+++ test/unit/pair_test.cpp
@@ -1,6 +1,7 @@
 #include <utility>
 #include <vector>
 #include <algorithm>
+#include <string>
 
 #include "cppunit/cppunit_proxy.h"
 
@@ -8,28 +9,38 @@
 using namespace std;
 #endif
 
-//
-// TestCase class
-//
 class PairTest : public CPPUNIT_NS::TestCase
 {
-  CPPUNIT_TEST_SUITE(PairTest);
-  CPPUNIT_TEST(pair0);
-  CPPUNIT_TEST_SUITE_END();
+    CPPUNIT_TEST_SUITE(PairTest);
+    CPPUNIT_TEST(pair0);
+    CPPUNIT_TEST(init);
+    CPPUNIT_TEST_SUITE_END();
 
-protected:
-  void pair0();
+  protected:
+    void pair0();
+    void init();
 };
 
 CPPUNIT_TEST_SUITE_REGISTRATION(PairTest);
 
-//
-// tests implementation
-//
 void PairTest::pair0()
 {
   pair<int, int> p = make_pair(1, 10);
 
   CPPUNIT_ASSERT(p.first==1);
   CPPUNIT_ASSERT(p.second==10);
+}
+
+void PairTest::init()
+{
+  pair<int, string> PAIR_ARRAY[] = { pair<int, string>(0, "0") };
+
+  int PAIR_ARRAY_SIZE = sizeof(PAIR_ARRAY) > 0 ? sizeof(PAIR_ARRAY) / sizeof(PAIR_ARRAY[0]) : 0;
+
+
+  for ( int i = 0; i < PAIR_ARRAY_SIZE; i++ ) {
+    CPPUNIT_CHECK( PAIR_ARRAY[i].first == 0 );
+    CPPUNIT_CHECK( PAIR_ARRAY[i].second == "0" );
+    PAIR_ARRAY[i].second = "1";
+  }
 }
