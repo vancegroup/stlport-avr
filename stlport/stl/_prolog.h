c3d29238e752f8098c97e7f0a4838a569b02ef81
dums <dums@01b27279-081b-0410-8cf9-d9d9e0fb0389>
2008-01-10 20:43:55 +0000

    Avoid code duplication by including _cprolog.h.
    git-svn-id: https://stlport.svn.sourceforge.net/svnroot/stlport/trunk/STLport@3375 01b27279-081b-0410-8cf9-d9d9e0fb0389

--- stlport/stl/_prolog.h
+++ stlport/stl/_prolog.h
@@ -4,25 +4,7 @@
  * allow reentrancy of this header.
  */
 
-#ifdef std
-#  undef std /* We undef "std" on entry , as STLport headers may include native ones. */
-#endif
-
-#ifdef _STLP_PROLOG_HEADER_INCLUDED
-#  error STlport prolog header can not be reincluded as long as epilog has not be included.
-#endif
-
-#define _STLP_PROLOG_HEADER_INCLUDED
-
-#ifndef _STLP_FEATURES_H
-#  include <stl/config/features.h>
-#endif
-
-/* If the platform provides any specific prolog actions,
- * like #pragmas, do include platform-specific prolog file */
-#if defined (_STLP_HAS_SPECIFIC_PROLOG_EPILOG)
-#  include <stl/config/_prolog.h>
-#endif
+#include <stl/_cprolog.h>
 
 /* Get all debug things, potentially only empty macros if none of
  * the debug features available in user config file is activated. */
