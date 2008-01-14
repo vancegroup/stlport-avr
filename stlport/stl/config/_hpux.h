95b7347283fa836eafc8c4abbb59eac112f76559
complement <complement@01b27279-081b-0410-8cf9-d9d9e0fb0389>
2008-01-14 21:36:29 +0000

    define _POSIX_C_SOURCE 199506
    git-svn-id: https://stlport.svn.sourceforge.net/svnroot/stlport/trunk/STLport@3391 01b27279-081b-0410-8cf9-d9d9e0fb0389

--- stlport/stl/config/_hpux.h
+++ stlport/stl/config/_hpux.h
@@ -7,7 +7,12 @@
 
 #ifdef __GNUC__
 #  define _STLP_NO_WCHAR_T
+#  define _STLP_NO_CWCHAR
 #  define _STLP_NO_LONG_DOUBLE
+#endif
+
+#ifndef _POSIX_C_SOURCE
+#  define _POSIX_C_SOURCE 199506
 #endif
 
 #endif /* __stl_config__hpux_h */
