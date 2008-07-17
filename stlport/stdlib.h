1ef511b23a3a6c5e8a606f253a365575e66a06c6
dums <dums@01b27279-081b-0410-8cf9-d9d9e0fb0389>
2008-07-17 19:32:50 +0000

    Fix errno workaround for latest MinGW gcc version.
    git-svn-id: https://stlport.svn.sourceforge.net/svnroot/stlport/trunk/STLport@3610 01b27279-081b-0410-8cf9-d9d9e0fb0389

--- stlport/stdlib.h
+++ stlport/stdlib.h
@@ -26,10 +26,9 @@
 #    define _STLP_DONT_POP_HEADER_ID
 #  endif
 
-#  if !defined (errno) && \
-      (defined (_MSC_VER) || (defined (__GNUC__) && defined (__MINGW32__)) || \
+#  if defined (_STLP_MSVC_LIB) || (defined (__GNUC__) && defined (__MINGW32__)) || \
        defined (__BORLANDC__) || defined (__DMC__) || \
-       (defined (__HP_aCC) && defined (_REENTRANT)))
+       (defined (__HP_aCC) && defined (_REENTRANT))
 /* Native stdlib.h contains errno macro definition making inclusion of native
  * errno.h in STLport errno.h impossible. We are then forced to include errno.h
  * first.
