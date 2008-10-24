ac324b31fde762d256fa3230ef32c514f007252e
Petr Ovtchenkov <ptr@void-ptr.info>
2008-10-25 00:32:06 +0400

    errno/assert problem, associated with #include_next in gcc 4.3.x
    If errno has been defined before inclusion of native errno.h
    including it from STLport errno.h becomes impossible because if:
      #define errno foo
    then
      #include _STLP_NATIVE_C_HEADER(errno.h)
    becomes:
      #include _STLP_NATIVE_C_HEADER(foo.h)
    To fix this problem you have to find where this definition comes
    from and include errno.h before it.
    
    The same issue applicable for assert/assert.h
    
    errno and assert may be defined without include stlport/errno.h;
    for example, in gcc 4.3.x in cassert we see
    
      #include_next <assert.h>
    
    This will include /usr/include/assert.h, not stlport/assert.h.
    
    To prevent problem,
    
      - don't consider #include directive if assert/errno defined,
        but don't report error too---usage /usr/include/assert.h
        is acceptable;
    
      - use in stlport/cassert #include <assert.h> instead of
        #include_next <cassert>; pay attention, that used #include,
        not #include_next.
    
    Test should be added ASAP.

--- stlport/errno.h
+++ stlport/errno.h
@@ -27,33 +27,18 @@
 #  endif
 #else
 #  ifndef errno
-/* We define the following macro first to guaranty the header reentrancy: */
-#    define _STLP_NATIVE_ERRNO_H_INCLUDED
 #    if defined (_STLP_HAS_INCLUDE_NEXT)
 #      include_next <errno.h>
 #    else
 #      include _STLP_NATIVE_C_HEADER(errno.h)
 #    endif
-#    if defined (__BORLANDC__) && (__BORLANDC__ >= 0x590) && defined (__cplusplus)
+#  endif
+#  if defined (__BORLANDC__) && (__BORLANDC__ >= 0x590) && defined (__cplusplus)
 _STLP_BEGIN_NAMESPACE
 using _STLP_VENDOR_CSTD::__errno;
 _STLP_END_NAMESPACE
-#    endif
 #  endif /* errno */
 
-#  if !defined (_STLP_NATIVE_ERRNO_H_INCLUDED)
-/* If errno has been defined before inclusion of native errno.h including it from STLport errno.h
- * becomes impossible because if:
- * #define errno foo
- * then
- * #include _STLP_NATIVE_C_HEADER(errno.h)
- * becomes:
- * #include _STLP_NATIVE_C_HEADER(foo.h)
- *
- * To fix this problem you have to find where this definition comes from and include errno.h before it.
- */
-#    error errno has been defined before inclusion of errno.h header.
-#  endif
 #endif
 
 #if (_STLP_OUTERMOST_HEADER_ID == 0x205)
@@ -63,8 +48,3 @@ _STLP_END_NAMESPACE
 #  endif
 #  undef  _STLP_DONT_POP_HEADER_ID
 #endif
-
-/* Local Variables:
- * mode: C
- * End:
- */
