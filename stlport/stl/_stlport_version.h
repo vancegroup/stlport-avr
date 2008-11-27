50dfa21129aa18444680ff190f412ecdf0f02f4d
Petr Ovtchenkov <ptr@void-ptr.info>
2008-11-27 21:51:22 +0300

    Bump patchlevel for development: road to STLport 5.2.1

--- stlport/stl/_stlport_version.h
+++ stlport/stl/_stlport_version.h
@@ -23,8 +23,8 @@
 /* STLport version */
 #define _STLPORT_MAJOR 5
 #define _STLPORT_MINOR 2
-#define _STLPORT_PATCHLEVEL 0
+#define _STLPORT_PATCHLEVEL 1
 
-#define _STLPORT_VERSION 0x520
+#define _STLPORT_VERSION 0x521
 
 #endif /* _STLP_STLPORT_VERSION_H */
