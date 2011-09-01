c2babbad50028e08b5ad2b5d8f30af9285963205
Andrey Nikolaynnov <nikolaynnov@users.sourceforge.net>
2011-09-01 09:20:50 +0400

    MSVS 2010 has abs
    MSVC starting with 2010 already define abs in global namespace

--- stlport/stl/_cstdlib.h
+++ stlport/stl/_cstdlib.h
@@ -154,7 +154,7 @@ inline lldiv_t div(_STLP_LONG_LONG __x, _STLP_LONG_LONG __y) { return _STLP_VEND
 inline _STLP_LONG_LONG  abs(_STLP_LONG_LONG __x) { return ::llabs(__x); }
 inline lldiv_t div(_STLP_LONG_LONG __x, _STLP_LONG_LONG __y) { return ::lldiv(__x, __y); }
 #    endif
-#  else
+#  elif  !defined (__ARMCC_VERSION) && (!defined (_STLP_MSVC_LIB) || (_STLP_MSVC_LIB < 1600))
 inline _STLP_LONG_LONG  abs(_STLP_LONG_LONG __x) { return __x < 0 ? -__x : __x; }
 #  endif
 #endif
