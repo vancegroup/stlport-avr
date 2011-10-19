5e6ded774dcf8da46968f5b848ee9b88bcd63b71
Petr Ovtchenkov <ptr@void-ptr.info>
2011-10-19 21:26:25 +0400

    raw_storage_iterator
    Remove surplus defines.

--- stlport/stl/_raw_storage_iter.h
+++ stlport/stl/_raw_storage_iter.h
@@ -36,35 +36,36 @@
 
 _STLP_BEGIN_NAMESPACE
 
-template <class _ForwardIterator, class _Tp>
-class raw_storage_iterator
-      : public iterator<output_iterator_tag,void,void,void,void>
+template <class _OutputIterator, class _Tp>
+class raw_storage_iterator :
+    public iterator<output_iterator_tag,void,void,void,void>
 {
-protected:
-  _ForwardIterator _M_iter;
-public:
-  typedef output_iterator_tag iterator_category;
-# ifdef _STLP_CLASS_PARTIAL_SPECIALIZATION
-  typedef void                value_type;
-  typedef void                difference_type;
-  typedef void                pointer;
-  typedef void                reference;
-# endif
-  explicit raw_storage_iterator(_ForwardIterator __x) : _M_iter(__x) {}
-  raw_storage_iterator<_ForwardIterator, _Tp>& operator*() { return *this; }
-  raw_storage_iterator<_ForwardIterator, _Tp>& operator=(const _Tp& __element) {
-    ::new (static_cast<void*>(&*_M_iter)) _Tp( __element );
-    return *this;
-  }
-  raw_storage_iterator<_ForwardIterator, _Tp>& operator++() {
-    ++_M_iter;
-    return *this;
-  }
-  raw_storage_iterator<_ForwardIterator, _Tp> operator++(int) {
-    raw_storage_iterator<_ForwardIterator, _Tp> __tmp = *this;
-    ++_M_iter;
-    return __tmp;
-  }
+  public:
+    explicit raw_storage_iterator( _OutputIterator __x ) :
+        _M_iter(__x)
+      { }
+
+    raw_storage_iterator<_OutputIterator, _Tp>& operator*()
+      { return *this; }
+    raw_storage_iterator<_OutputIterator, _Tp>& operator=( const _Tp& __element )
+      {
+        ::new (static_cast<void*>(&*_M_iter)) _Tp( __element );
+        return *this;
+      }
+    raw_storage_iterator<_OutputIterator, _Tp>& operator++()
+      {
+        ++_M_iter;
+        return *this;
+      }
+    raw_storage_iterator<_OutputIterator, _Tp> operator++(int)
+      {
+        raw_storage_iterator<_OutputIterator, _Tp> __tmp = *this;
+        ++_M_iter;
+        return __tmp;
+      }
+
+  protected:
+    _OutputIterator _M_iter;
 };
 
 _STLP_END_NAMESPACE
