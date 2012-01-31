2cc9123410069811990137b5ca139c9592debdc6
Petr Ovtchenkov <ptr@void-ptr.info>
2012-01-31 15:01:05 +0400

    Prototype for <atomic>
    Basic interfaces only. Not really implemented.

---
+++ test/unit/atomic_test.cc
@@ -0,0 +1,18 @@
+// -*- C++ -*- Time-stamp: <2012-01-06 12:07:39 ptr>
+
+/*
+ * Copyright (c) 2011
+ * Petr Ovtchenkov
+ *
+ * This material is provided "as is", with absolutely no warranty expressed
+ * or implied. Any use is at your own risk.
+ *
+ * Permission to use or copy this software for any purpose is hereby granted
+ * without fee, provided the above notices are retained on all copies.
+ * Permission to modify the code and to distribute modified code is granted,
+ * provided the above notices are retained, and a notice that the code was
+ * modified is included with the above copyright notice.
+ *
+ */
+
+#include <atomic>
---
+++ test/unit/atomic_test.h
@@ -0,0 +1,31 @@
+// -*- C++ -*- Time-stamp: <2012-01-06 12:07:04 ptr>
+
+/*
+ * Copyright (c) 2011
+ * Petr Ovtchenkov
+ *
+ * This material is provided "as is", with absolutely no warranty expressed
+ * or implied. Any use is at your own risk.
+ *
+ * Permission to use or copy this software for any purpose is hereby granted
+ * without fee, provided the above notices are retained on all copies.
+ * Permission to modify the code and to distribute modified code is granted,
+ * provided the above notices are retained, and a notice that the code was
+ * modified is included with the above copyright notice.
+ *
+ */
+
+#ifndef __TEST_ATOMIC_TEST_H
+#define __TEST_ATOMIC_TEST_H
+
+#define __FIT_EXAM
+
+#include <exam/suite.h>
+
+class atomic_test
+{
+  public:
+  // int EXAM_DECL(align);
+};
+
+#endif // __TEST_ATOMIC_TEST_H
