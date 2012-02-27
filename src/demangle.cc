6f0ba54dff7ac1be0cec2a886be56cbfa215d430
Petr Ovtchenkov <ptr@void-ptr.info>
2012-02-27 17:57:16 +0400

    STLport's defines and includes for <thread> and friends
    Prepare for merge into mainstream and STLport-5.2.
    
    Not useful usage yet, but next work is differrent
    for mainstream and STLport-5.2.

--- src/demangle.cc
+++ src/demangle.cc
@@ -1,10 +1,17 @@
 // -*- C++ -*- Time-stamp: <09/06/01 01:06:36 ptr>
 
 /*
- * Copyright (c) 2009, 2010
+ * Copyright (c) 2009-2012
  * Petr Ovtchenkov
  *
- * Licensed under the Academic Free License version 3.0
+ * This material is provided "as is", with absolutely no warranty expressed
+ * or implied. Any use is at your own risk.
+ *
+ * Permission to use or copy this software for any purpose is hereby granted
+ * without fee, provided the above notices are retained on all copies.
+ * Permission to modify the code and to distribute modified code is granted,
+ * provided the above notices are retained, and a notice that the code was
+ * modified is included with the above copyright notice.
  *
  */
 
