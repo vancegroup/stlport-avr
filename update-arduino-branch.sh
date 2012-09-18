cd5e781879289a87edfd1fbe11f54e608b25f4be
Ryan Pavlik <rpavlik@iastate.edu>
2012-09-18 13:51:14 -0500

    OK, script actually works!

--- update-arduino-branch.sh
+++ update-arduino-branch.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -x
 set -e
 
+devbranch=STLport-5.2-avr
 installbranch=STLport-5.2-arduino-installed
 builddir=build/lib
 
@@ -12,11 +13,12 @@ trap "rm -rf $TMPDIR"  EXIT
 git clone $basedir $TMPDIR/repo
 (
 	cd $TMPDIR/repo
+	git checkout $devbranch || true
 	./configure --target=avr
 	make -C $builddir SKETCHBOOK=$TMPDIR install-arduino
 	git checkout -b $installbranch origin/$installbranch
 	git merge --no-ff --no-commit $devbranch || true
-	git rm rf *
+	git rm -rf *
 	mv $TMPDIR/libraries/stlport/* .
 	git add .
 	git commit -m "Generated installed version for Arduino"
