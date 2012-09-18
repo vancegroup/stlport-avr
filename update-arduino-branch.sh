f91b31343304d40d847741c24b1dc509993019f4
Ryan Pavlik <rpavlik@iastate.edu>
2012-09-18 15:34:10 -0500

    More work on script

--- update-arduino-branch.sh
+++ update-arduino-branch.sh
@@ -18,7 +18,8 @@ git clone $basedir $TMPDIR/repo
 	make -C $builddir SKETCHBOOK=$TMPDIR install-arduino
 	git checkout -b $installbranch origin/$installbranch
 	git merge --no-ff --no-commit $devbranch || true
-	git rm -rf *
+	rm -rf *
+	git read-tree $devbranch
 	mv $TMPDIR/libraries/stlport/* .
 	git add .
 	git commit -m "Generated installed version for Arduino"
