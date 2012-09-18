a32cb2de3ccdf88c310070822ccd4832c84643de
Ryan Pavlik <rpavlik@iastate.edu>
2012-09-18 13:48:26 -0500

    more work on script

--- update-arduino-branch.sh
+++ update-arduino-branch.sh
@@ -12,11 +12,12 @@ trap "rm -rf $TMPDIR"  EXIT
 git clone $basedir $TMPDIR/repo
 (
 	cd $TMPDIR/repo
-	make -C $builddir SKETCHBOOK=$tmpdir install-arduino
+	./configure --target=avr
+	make -C $builddir SKETCHBOOK=$TMPDIR install-arduino
 	git checkout -b $installbranch origin/$installbranch
 	git merge --no-ff --no-commit $devbranch || true
 	git rm rf *
-	mv $tmpdir/libraries/stlport/* .
+	mv $TMPDIR/libraries/stlport/* .
 	git add .
 	git commit -m "Generated installed version for Arduino"
 	git push origin $installbranch
