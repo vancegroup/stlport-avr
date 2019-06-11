64dba09c1301a8fd7059a789a7444602a64d4c54
Jongpil Kim <sragin@naver.com>
2019-06-11 15:04:44 +0900

    Update _gcc.h
    fixed bug in 'nullptr_t' check macro

--- stl/config/_gcc.h
+++ stl/config/_gcc.h
@@ -237,7 +237,7 @@ typedef unsigned int wint_t;
 #    define _STLP_VARIADIC_TEMPLATES
 #    define _STLP_CPP_0X
 #  endif
-#  if (__GNUC__ > 4) || ((__GNUC__ == 4) && (__GNUC_MINOR__ < 6))
+#  if (__GNUC__ < 4) || ((__GNUC__ == 4) && (__GNUC_MINOR__ < 6))
 #    define _STLP_NO_NULLPTR_T
 #  endif
 #  if (__GNUC__ > 4) || ((__GNUC__ == 4) && (__GNUC_MINOR__ > 5))
